8187d84331b7240b16aa1058d5cd089a7a9b2147
Amir Poolad <amirhossein.poolad@gmail.com>
2026-07-03 15:09:31 -0400

    Exclude unused wire types from lookahead sampling point calculation

--- vpr/src/route/router_lookahead/router_lookahead_map.cpp
+++ vpr/src/route/router_lookahead/router_lookahead_map.cpp
@@ -545,7 +545,11 @@ static void compute_router_wire_lookahead(const std::vector<t_segment_inf>& segm
 
     int longest_seg_length = 0;
     for (const t_segment_inf& seg_inf : segment_inf_vec) {
-        longest_seg_length = std::max(longest_seg_length, seg_inf.length);
+        // longest_seg_length only cares about segments that exist in normal routing wires
+        // Exclude segment types with 0 frequency e.g. scatter gather segments and unused segments
+        if (seg_inf.frequency != 0) {
+            longest_seg_length = std::max(longest_seg_length, seg_inf.length);
+        }
     }
 
     // Profile each wire segment type
